d38cc7e5eb1e7f379d678f20a6cc83d599b8dde4
Katharine Fernandes <70667492+katharinefernandes@users.noreply.github.com>
2021-10-17 21:49:12 -0300

    :sparkles: Atividade 13 - AW1

--- Atividades/A13/js/main.js
+++
@@ -1,9 +0,0 @@
-const modalBox = document.querySelector('.modal-overlay')
-const Modal = {
-	open() {
-		modalBox.classList.add('active')
-	},
-	close() {
-		modalBox.classList.remove('active')
-	}
-}
