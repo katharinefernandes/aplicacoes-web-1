23b05833a0753a1420bb701cea0f07a625120b39
Katharine Fernandes <70667492+katharinefernandes@users.noreply.github.com>
2021-10-26 11:51:23 -0300

    :truck: Updates

--- Atividades/A17/script.js
+++ Atividades/A17/script.js
@@ -29,3 +29,57 @@ document.querySelectorAll("input").forEach(($input) => {
     false
   );
 });
+
+const form = document.getElementById('form')
+const email = document.getElementById('email')
+const telefone = document.getElementById('telefone')
+const cpf = document.getElementById('cpf')
+
+const button = document.getElementById('button')
+
+button.addEventListener('click', event => {
+    event.preventDefault()
+
+    if (email.value == '') {
+        email.classList.add('errorInput')
+    } else {
+        email.classList.remove('errorInput')
+    }
+
+    if (telefone.value == '') {
+        telefone.classList.add('errorInput')
+    } else {
+        telefone.classList.remove('errorInput')
+    }
+
+    if (cpf.value == '') {
+        cpf.classList.add('errorInput')
+    } else {
+        cpf.classList.remove('errorInput')
+    }
+
+    // Validar o e-mail
+    if (
+        email.value.indexOf('@') == -1 ||
+        email.value.indexOf('.') == -1 ||
+        email.value.indexOf('.') - email.value.indexOf('@') == 1
+    ) {
+        email.classList.add('errorInput')
+    } else {
+        email.classList.remove('errorInput')
+    }
+
+    // Validar o cpf
+    if (cpf.value.length <= 13) {
+        cpf.classList.add('errorInput')
+    } else {
+        cpf.classList.remove('errorInput')
+    }
+
+    // Validar o telefone
+    if (telefone.value.length <= 13) {
+        telefone.classList.add('errorInput')
+    } else {
+        telefone.classList.remove('errorInput')
+    }
+})
